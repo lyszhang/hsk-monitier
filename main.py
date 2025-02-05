c1a5b4bf8deb445927a05c44e1af599111269497
lyszhang <lyszhang@126.com>
2025-02-05 03:32:56 +0000

    feat: 指定日期范围的数据汇总

--- main.py
+++ main.py
@@ -4,20 +4,34 @@ from fee import get_fee_income_info
 from batch import get_accounts_info_daily, get_contract_info_daily, get_tx_info_daily
 
 def main():
-    # 替换为预定义日期，避免使用 input
-    # 使用昨天的日期作为目标日期
-    print("HashKey Chain日常活动扫描")
-    target_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-    # target_date = "2024-12-21"  # 示例日期
-    print("扫描日期:",target_date)
-    # get_fee_income_info(target_date)
-
     blockscout_url = "https://explorer.hsk.xyz/api/v1/lines"
     etherscan_url = "https://api.etherscan.io/v2/api"
     gateio_market_url = "https://api.gateio.ws/api/v4"
-    get_accounts_info_daily(blockscout_url, target_date)
-    get_tx_info_daily(blockscout_url, etherscan_url, gateio_market_url, target_date)
-    get_contract_info_daily(blockscout_url, target_date)
+
+    # 替换为预定义日期，避免使用 input
+    # 使用昨天的日期作为目标日期
+    # print("HashKey Chain日常活动扫描")
+    # target_date = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    # # target_date = "2024-12-21"  # 示例日期
+    # print("扫描日期:",target_date)
+
+    # # work, work
+    # get_accounts_info_daily(blockscout_url, target_date)
+    # get_tx_info_daily(blockscout_url, etherscan_url, gateio_market_url, target_date)
+    # get_contract_info_daily(blockscout_url, target_date)
+
+    # 设置起始日期和结束日期
+    start_date = datetime.datetime(2024, 12, 16)
+    end_date = datetime.datetime(2025, 1, 22)
+
+    # 初始化当前日期
+    current_date = start_date
+
+    # 循环打印日期，直到结束日期
+    while current_date <= end_date:
+        current_date_str = current_date.strftime("%Y-%m-%d")
+        get_tx_info_daily(blockscout_url, etherscan_url, gateio_market_url, current_date_str)
+        current_date += datetime.timedelta(days=1)
 
 if __name__ == "__main__":
     main()
